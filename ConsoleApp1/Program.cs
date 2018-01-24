8351a68d4901469698d53cfb6d932ef6a045aa11
Artem Petrushenko <ampetrushenko@gmail.com>
2018-01-25 00:01:41 +0200

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -12,9 +13,9 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Type Some Text");
             string Text = Console.ReadLine();
-            Console.WriteLine($"This is your text: {Text}");
+            Console.WriteLine($"This is your text: \"{Text}\"!\nEnd...");
         }
-        static void Main(string[] args)
+        public static void Start()
         {
             Console.WriteLine("Select task:");
             byte TaskNumber = byte.Parse(Console.ReadLine());
@@ -26,6 +27,14 @@ namespace ConsoleApp1
                 default:
                     Console.WriteLine("Something wen wrong!");
                     break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            DriveInfo[] Drivers = DriveInfo.GetDrives();
+            foreach (DriveInfo Driver in Drivers)
+            {
+                Console.WriteLine($"Disk: {Driver.Name}. Available space: {Math.Round((Driver.TotalFreeSpace * 0.000000001),2)}Gb of {Math.Round((Driver.TotalSize * 0.000000001),2)}Gb");
             }
         }
     }
